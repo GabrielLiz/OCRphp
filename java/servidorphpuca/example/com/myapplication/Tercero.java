7c69baa9222a6ec2eb10500cf179e65a1eb2876d
GabrielLiz89 <gilberth89@gmail.com>
2014-07-10 18:32:18 +0200

    Clieaning Code

--- java/servidorphpuca/example/com/myapplication/Tercero.java
+++ java/servidorphpuca/example/com/myapplication/Tercero.java
@@ -26,7 +26,7 @@ public class Tercero extends Activity {
 		  super.onCreate(savedInstanceState);
 		  setContentView(R.layout.activity_tercero);
 		        
-		  //Instancia de gridview
+
 		  gvImages = (GridView) findViewById(R.id.gv_images);
 		  
 	      
@@ -43,103 +43,49 @@ public class Tercero extends Activity {
         gvImages.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                     Log.i("gol",mAdapter.getItem(position).toString());
 
                     Intent imsagefullsize= new Intent(getApplicationContext(), TerceroImageFullSize.class);
                     Bundle bs= new Bundle();
                     bs.putString("urls",mAdapter.getItem(position).toString());
-                    bs.putInt("position",position);
-                     imsagefullsize.putExtras(bs);
-                     startActivity(imsagefullsize);
+
+                    imsagefullsize.putExtras(bs);
+
+                    startActivity(imsagefullsize);
 
             }
         });
 
-		 
-		  
-		/*  
-		 * gvImages.setAdapter(new ImageAdapter(this));
-		  gvImages.setOnItemClickListener (new OnItemClickListener() {
-		
-			  public void onItemClick(AdapterView<?> parent, View v, int pos, long id) 
-			  {
-				  ImageView imageView= (ImageView) findViewById(R.id.image1);
-				  imageView.setImageResource(getItem[pos]);
-			  }
-		  });
-
-		}
-
-	public class ImageAdapter extends BaseAdapter {
-		private Context context;
-		private int itemBackground;
-		 
-		public ImageAdapter(Context c)
-		{
-			context= c;
-		}*
-
-		@Override
-		public int getCount() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public Object getItem(int position) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public long getItemId(int position) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		*/
-		
-		
-		
 	}
-		  
-		  /*
-		  
-		  mostrarTipoFichero.setAction(android.content.Intent.ACTION_VIEW);
-	       mostrarTipoFichero.setDataAndType(Uri.fromFile(ficheroSeleccionado), "image/jpg");
-	       startActivityForResult(mostrarTipoFichero, 0);
-	       
-	       */
-		  
+
+
 	
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         getMenuInflater().inflate(R.menu.tercero, menu);
         return true;
     }
-    
+
+
+
     
     private boolean isNetworkAvailable() {
-	    ConnectivityManager connectivityManager 
-	          = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+	    ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+
+	       return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	} 
 	
     
-	/*
-	 * AsyncTask that will download the xml file for us and store it locally.
-	 * After the download is done we'll parse the local file.
-	 */
+
+
 	private class SitesDownloadTask extends AsyncTask<Void, Void, Void>{
-		MiThread tr;
+
+        MiThread tr;
 		@Override
 		protected Void doInBackground(Void... arg0) {
 			//Download the file
--- java/servidorphpuca/example/com/myapplication/TerceroImageFullSize.java
+++ java/servidorphpuca/example/com/myapplication/TerceroImageFullSize.java
@@ -11,9 +11,6 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
-import com.nostra13.universalimageloader.utils.StorageUtils;
-
-import java.io.File;
 
 
 public class TerceroImageFullSize extends Activity{
@@ -21,38 +18,28 @@ public class TerceroImageFullSize extends Activity{
     ImageView imageVi;
     String url=null;
 
-    int pos;
+
     DisplayImageOptions options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_full_size);
         imageVi =(ImageView)findViewById(R.id.imagefullsize);
-        File s= StorageUtils.getCacheDirectory(this);
-           String data[]= s.list();
 
-            loader = SitesAdapter.loader();
 
+        loader = SitesAdapter.loader();
 
         Bundle extras = getIntent().getExtras();
 
         if (extras != null) {
 
-            pos=  extras.getInt("position");
+
             url=extras.getString("urls");
         }
 
         options = new DisplayImageOptions.Builder()
                 .showImageForEmptyUri(R.drawable.ic_launcher).cacheOnDisk(true).cacheInMemory(false)
                 .build();
-
-
-        String path = "file:///"+s.getPath()+"/"+data[pos];
-
-
-
-
-
 
 
 
