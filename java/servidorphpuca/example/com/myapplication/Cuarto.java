edf791bc02e7f12858d39277de74a1f42a68e278
¨gabrielliz¨ <gilberth89@gmail.com>
2014-07-28 23:21:36 +0200

    ¨secondcommit¨

--- java/servidorphpuca/example/com/myapplication/Cuarto.java
+++ java/servidorphpuca/example/com/myapplication/Cuarto.java
@@ -19,6 +19,7 @@ public class Cuarto extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		// this is the test pushing
+		// keeptesting
 		getMenuInflater().inflate(R.menu.cuarto, menu);
 		return true;
 	}
